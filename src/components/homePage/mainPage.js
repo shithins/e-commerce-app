32984c7e1e72ffd5ee2387d8e22118acbc475b65
ckvaizz <vaisakh.k591@gmail.com>
2021-04-29 19:12:35 +0530

    started

---
+++ src/components/homePage/mainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const mainPage = () => {
+    return(
+        <>
+            
+        </>
+    );
+
+}
+
+export default mainPage;
---
+++ src/components/navBar/navBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './navBar.css';
+import PageviewIcon from '@material-ui/icons/Pageview';
+import HomeIcon from '@material-ui/icons/Home';
+
+const navBar = () => {
+
+
+    return(
+       <div className="main">
+            <h1 >E-COM</h1>
+            <div style={{flex:".5"}}>
+            <PageviewIcon style={{height:"43px",width:"43px",margin:"10px",color:"white"}}/>
+            <HomeIcon style={{height:"43px",width:"43px",margin:"10px",color:"white",marginTop:"25px"}}/>
+            </div>
+         </div>
+
+
+    )
+
+}
+
+
+export default navBar
