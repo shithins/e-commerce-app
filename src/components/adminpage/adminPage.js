70fcac863ab6d08a76a8daa29cb5354cd2debfc5
shithins <shithinckss2001@gmail.com>
2021-05-03 09:18:03 +0530

    admin page table started

--- src/components/adminpage/adminPage.js
+++ src/components/adminpage/adminPage.js
@@ -9,7 +9,28 @@ const Adminpage = () => {
                 
            </div>
             <div className="top-bar">
-
+                <h3>ORDERS</h3>
+                <h3>STOCKS</h3>
+                <h3>ADD PRODUCT</h3>
+            </div>
+            <div className="ad-table">
+                <table>
+                    <tr>
+                    <th>Recent Orders</th>
+                    </tr>
+                    <tr>
+                        <th>
+                            Product ID
+                        </th>
+                        <th>Buyer Name</th>
+                        <th>Address</th>
+                        <th>Order Date</th>
+                        <th>Delivery Date</th>
+                        <th>Payment Method</th>
+                        <th>Status</th>
+                        <th>Price</th>
+                    </tr>
+                </table>
             </div>
        </div>
 
