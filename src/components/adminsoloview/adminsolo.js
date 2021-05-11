734bca8e5ede0bbffc1ecfa64de1ad5aab3e991c
shithins <shithinckss2001@gmail.com>
2021-05-11 10:31:41 +0530

    admin single order page designed

--- src/components/adminsoloview/adminsolo.js
+++ src/components/adminsoloview/adminsolo.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import './adminsolo.css';
+import soloproductimg from '../../images/hoodies1.jpeg'
 
 const Adminsingle = () => {
     return(
 
         <div className="adminsolo-main">
-            <div className="order-details">
-                
+            <div className="adminsolo-order-details">
+                <img src={soloproductimg} alt="//"/>
+                <h2>Hoodies</h2>
+                <h3>Price :</h3>
+                <h3>Order Date :</h3>
+                <div className="customer-details">
+                    <h3>Name :</h3>
+                    <h3>Address :</h3>
+                    <h3>Payment method :</h3>
+                    <h3>Size :</h3>
+                    <h3>Email :</h3>
+                    <h3>Phone :</h3>
+                </div>
+               
             </div>
+            
         </div>
     )
 }
